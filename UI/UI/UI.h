d1226636777ac1e29fa38e9d069941100f02ea57
Molnar Marton <marton.molnar@otpbank.hu>
2022-05-14 12:40:42 +0200

    Create architecture

---
+++ UI/UI/UI.h
@@ -0,0 +1,18 @@
+//
+//  UI.h
+//  UI
+//
+//  Created by Molnar Marton on 2022. 05. 14..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UI.
+FOUNDATION_EXPORT double UIVersionNumber;
+
+//! Project version string for UI.
+FOUNDATION_EXPORT const unsigned char UIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UI/PublicHeader.h>
+
+
